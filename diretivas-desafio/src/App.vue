5b3537d6e7777be9f3dd6021cc16371e48dfb733
Ricardo Falcades <ricardo.f@evencard.com.br>
2022-01-27 08:53:17 -0300

    desafio diretivas

---
+++ diretivas-desafio/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+	<div id="app">
+		<h1>Diretivas (Desafio)</h1>
+		<hr>
+		<!-- Exercício -->
+		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+		<button v-quando:click="acao">Executar</button>
+		<p v-quando:mouseenter="mouseEnter">Teste mouse event</p>
+	</div>
+</template>
+
+<script>
+
+//import func from 'vue-editor-bridge';
+
+export default {
+	directives:{
+		quando:{
+			bind(el, binding) {
+				// el.onclick = function(e) {
+				// 	binding.value()
+				// }
+				const tipo = binding.arg
+				const fn = binding.value
+				el.addEventListener(tipo, fn)
+
+			}
+		}
+	},
+	methods:{
+		acao(){
+			alert('Executada!');
+		},
+		mouseEnter(){
+			alert('Mouse!');
+		},
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+button {
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+</style>
